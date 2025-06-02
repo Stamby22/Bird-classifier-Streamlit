6b538285e1287ca2b56fad0712afd4be190b5a8c
stamby22 <paste@volny.cz>
2025-06-02 16:21:11 +0200

    convert to streamlit + add classify_image.ipynb

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import streamlit as st
 from PIL import Image
 import keras # tensorflow as tf
+from keras.models import load_model
 from keras.preprocessing import image as kimage
-from keras.applications.efficientnet_v2 import decode_predictions
+
+#from keras.ops import expand_dims
+from numpy import argmax, expand_dims
 import os
 import json
 
-IMG_SIZE = 300
+
 # Loading the dictionary from the JSON file
 current_working_directory = os.getcwd()
 model_directory = os.path.join(current_working_directory, 'Models')
@@ -14,26 +17,43 @@ model_directory = os.path.join(current_working_directory, 'Models')
 bird_classes_file_name = os.path.join(model_directory, "bird_classes.json")
 with open(bird_classes_file_name, 'r') as json_file:
     bird_classes = json.load(json_file)
-
-bird_species = {v: k for k, v in bird_classes.items()}
+bird_species = {int(k): v for k, v in bird_classes.items()}
 
 # load trained model
 model_file_name = os.path.join(model_directory, "Bird_EffiB3_DS7_FT.keras")
-MODEL = keras.models.load_model(model_file_name)
+MODEL = load_model(model_file_name)
 
 def classify_image(image):
+    IMG_SIZE = 300
     image = image.convert("RGB")
-    image = image.resize(IMG_SIZE, IMG_SIZE)
+    image = image.resize((IMG_SIZE, IMG_SIZE))
 
     # Preprocess the image
     img_array = kimage.img_to_array(image)
-    img_array = keras.expand_dims(img_array, 0)
+    img_array = expand_dims(img_array, 0)
 
     predictions = MODEL.predict(img_array)
-  
-    decoded_predictions = decode_predictions(predictions)[0]
-    return decoded_predictions
-  
+    spec_pred = argmax(predictions, axis=1)
+
+    prob_list = get_probabilities(predictions[0], 0.1)
+
+    return spec_pred, prob_list
+
+# get probabilities of the best predictions
+def get_probabilities(prob_list, trashold):
+    #prob_list = model_labels[ind]
+    prob_dict = {}
+    i = 0
+    for prob in prob_list:
+        prob_dict[round(100*prob)] = str(bird_species[i]).replace('_', ' ')
+        i += 1
+    # myKeys = list(prob_dict.keys())
+    myKeys = list(num for num in prob_dict.keys() if num > trashold)
+    myKeys.sort(reverse = True)
+    # myKeys = myKeys[:num_best]  # [cislo for cislo in cisla if cislo > 30]
+    sorted_list = [str(i)+'% '+ prob_dict[i] for i in myKeys]
+    return sorted_list
+
 def main():
     st.title("Bird Image Classification")
 
@@ -43,11 +63,15 @@ def main():
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
-        predictions = classify_image(image)
+        spec_id, predictions = classify_image(image)
+
+        st.subheader("Name:")
+
+        st.write(str(bird_species[int(spec_id)]).replace('_', ' '))
 
         st.subheader("Classification Results:")
-        for i, (imagenet_id, label, score) in enumerate(predictions):
-            st.write(f"{i + 1}: {label} ({score:.2f})")
+        for i, text in enumerate(predictions):
+            st.write(f"{i + 1}. {text}")
 
 if __name__ == "__main__":
     main()
